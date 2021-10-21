ea8fcd6611d3b0549b0759b4bd7297e772a96da1
henriqueomatos <aghenriquematos@gmail.com>
2021-10-21 00:01:27 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*maine é um método autoexecutável em Java*/
+	public static void main(String[] args) {
+		
+		/*escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java do Curso Formação Java Web");
+		System.out.println("Java é orientado a objetos");
+	}
+
+}
