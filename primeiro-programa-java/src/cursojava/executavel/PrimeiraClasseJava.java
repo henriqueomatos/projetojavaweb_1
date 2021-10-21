5489c2dec78da504a0156ccbc89e6de4ee19372c
henriqueomatos <aghenriquematos@gmail.com>
2021-10-21 00:42:56 -0300

    Modifiquei um comentário

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*maine é um método autoexecutável em Java*/
+	/*main é um método autoexecutável em Java*/
 	public static void main(String[] args) {
 		
 		/*escreve texto no console*/
